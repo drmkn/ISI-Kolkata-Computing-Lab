ddc1df11fe6c44c042a02190c5f6f61e581690e0
Dhruv Rathore <81362311+drmkn@users.noreply.github.com>
2023-09-13 17:52:33 +0530

    Add files via upload

---
+++ Intro to C/13sept/define.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#define BUF_LEN 100
+#define INCR ++
+#define SQR(X) X*X
+#define SQR1(x) x*x+1
+#define Malloc(n,type) (type*)malloc(n*sizeof(type)
+int main(){
+    /*int x[BUF_LEN+0.1];
+    int a[1BUF_LEN];
+    int b[BUF_LEN00];
+    printf("%ld\n",sizeof(x));
+    printf("%ld\n",sizeof(a));
+    printf("%ld\n",sizeof(b));*/
+    /*int a =1;
+    a INCR;
+    printf("%d\n",a);*/
+    int x;
+    printf("%d %d\n", SQR1(5+1),SQR('1'));
+    printf("%d %d\n", SQR1(5),SQR1('1'));
+    
+
+}
