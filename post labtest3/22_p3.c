4f539f8790aeb8d2bf24806ee5d80db473640798
Dhruv Rathore <81362311+drmkn@users.noreply.github.com>
2023-12-12 18:20:52 +0530

    Add files via upload

---
+++ post labtest3/22_p3.c
@@ -0,0 +1,160 @@
+#include "common.h"
+
+
+#define NUM_SYMS 26
+
+typedef unsigned long int TRIE_NODE[NUM_SYMS + 1]; /* the last field stores how 
+                                               * many times this string has
+                                               * occurred as a complete word
+                                               */
+
+
+
+unsigned long max_nodes, num_nodes;
+TRIE_NODE *trie;
+
+int init_trie()
+{
+    max_nodes = 10000;
+    if (NULL == (trie = (TRIE_NODE *) calloc(max_nodes, sizeof(TRIE_NODE))))
+        ERR_MESG("init-trie: out of memory\n");
+    num_nodes = 1;
+    return 0;
+}
+
+int insert_node()
+{
+    if (num_nodes == max_nodes) {
+        max_nodes *= 2;
+        if (NULL == (trie = Realloc(trie, max_nodes, TRIE_NODE)))
+            ERR_MESG("insert-node: out of memory\n");
+        bzero((void *) (trie + num_nodes), num_nodes * sizeof(TRIE_NODE));
+    }
+    num_nodes++;
+    return num_nodes - 1;
+}
+
+int insert_string(char *s)
+{
+    unsigned int index = 0;
+    int c, new_index;
+
+    while (*s) {
+        c = *s;
+        if (c >= 'A' && c <= 'Z')
+            c = 'a' + c -'A';
+        if (c >= 'a' && c <= 'z') {
+            c = c - 'a';
+            if (trie[index][c] != 0)
+                /* just follow the pointer */
+                index = trie[index][c];
+            else {
+                /* need new node */
+                if (UNDEF == (new_index = insert_node()))
+                    return UNDEF;
+                index = trie[index][c] = new_index;
+            }                
+        }
+        else
+            fprintf(stderr, "Unexpected character %d\n", c);
+        s++;
+    }
+    trie[index][NUM_SYMS]++;
+
+    return 0;
+}
+
+typedef struct {
+    char c;
+    unsigned long index;
+    unsigned int level;
+} STACK_NODE;
+
+int trie_dfs()
+{
+    char word[BUF_LEN << 10], c;
+    unsigned int level;
+    int i;
+    unsigned long top = 1, index, count;
+    STACK_NODE *stack, *sptr;
+
+    if (NULL == (stack = Malloc(num_nodes, STACK_NODE)))
+        ERR_MESG("trie_dfs: out of memory");
+    sptr = stack;
+    sptr->c = '\0';
+    sptr->index = 0;
+    sptr->level = 0;
+    while (top > 0) {
+        sptr = stack + top - 1;
+        top--;
+        c = sptr->c;
+        index = sptr->index;
+        level = sptr->level;
+        count = trie[index][NUM_SYMS];
+        if (c) {
+            assert(level > 0);
+            word[level - 1] = c;
+        }
+        if (count > 0) {
+            word[level] = '\0';
+            printf("%s %lu\n", word, count);
+        }
+        for (i = NUM_SYMS - 1; i >= 0; i--) {
+            if (trie[index][i] > 0) {
+                sptr = stack + top;
+                sptr->c = 'a' + i;
+                sptr->index = trie[index][i];
+                sptr->level = level + 1;
+                top++;
+            }
+        }
+    }
+
+    return 0;
+}
+
+int dfs_search_word_count(int index){
+    int count = 0;
+    //for(int i = 0;i<26;i++) sum = trie[index][i]; 
+    //if(sum == 0) return trie[index][27];
+
+    for(int j = 0;j<27;j++){
+        if(trie[index][j] != 0){
+            count += dfs_search_word_count(trie[index][j]);
+        }
+        if(trie[index][27] != 0){
+            count += trie[index][27];
+        }
+    }
+    return count;
+}
+
+int main(int ac, char **av){
+    int w; char Buffer[256];
+    FILE *prefixes,*strings;
+    w = init_trie();
+    prefixes = fopen(av[1],"r");int n;
+    strings = fopen(av[2],"r");int m;
+    fscanf(prefixes,"%d",&n);fscanf(strings,"%d",&m);
+    for(int i = 0;i<m;i++){
+        fscanf(strings,"%s",Buffer);
+        w = insert_string(Buffer);
+    }
+    for(int j = 0;j<n;j++){
+        fscanf(prefixes,"%s",Buffer);int i = 0;
+        printf("%s",Buffer);char c = Buffer[i];int index = 0;
+        while(c != '\0'){
+            if(trie[index][c-'a'] != 0){
+                index = trie[index][c-'a'];
+            }
+            c = Buffer[++i];
+        }
+        
+        printf(" %d\n",dfs_search_word_count(index));
+    }
+
+
+
+
+    return 0;
+}
